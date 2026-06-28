3b33594a07b1125fcb403ac22852cabf0f241e78
Erallie <31375589+Erallie@users.noreply.github.com>
2026-06-28 12:33:44 -0700

    Revert "Starting to try to optimize phone for mobile"
    This reverts commit 9fc540ab1d94e01300477b2d104d6d09b068d436.

--- src/lib/components/iPhone.svelte
+++ src/lib/components/iPhone.svelte
@@ -13,21 +13,19 @@
 <style>
 	div.container {
 		position: relative;
-		padding: 1.6%;
-		width: min(675px, 90%);
-		aspect-ratio: 675 / 320;
+		padding: 20px;
+		width: 675px;
 		margin: 10px auto;
 	}
 	div.background {
 		background-color: black;
 		position: absolute;
-		top: 5.8%;
-		left: 2.9%;
-		width: calc(100% - 5.8%);
-		height: calc(100% - 11.6%);
+		top: 20px;
+		left: 20px;
+		width: calc(100% - 40px);
+		height: calc(100% - 40px);
 		z-index: 0;
-		border-radius: 6%;
-		/* border: 1px solid red; */
+		border-radius: 40px;
 	}
 	img {
 		position: absolute;
@@ -38,7 +36,5 @@
 	}
 	.iphone-contents {
 		isolation: isolate;
-		width: 100%;
-		height: 100%;
 	}
 </style>
